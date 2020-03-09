a6a6c3b1a56d9821009bb3cbabbc0e005ac79405
Charles Brunelle <charles.brunelle@ara-uas.com>
2020-03-09 16:17:46 -0400

    Alpha.

---
+++ src/main/java/com/demo/basicdeckofcards/BasicdeckofcardsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.basicdeckofcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicdeckofcardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicdeckofcardsApplication.class, args);
+    }
+
+}
